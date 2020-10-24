b0873a720ecb3e59c7b62861e64938e93cadb2ed
bryan <bryanjenks@protonmail.com>
2020-10-23 22:02:03 -0700

    Initial Commit

---
+++ Bryan_Jenks_WGU_C867_Task/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Bryan_Jenks_WGU_C867_Task
+//
+//  Created by bryan jenks on 10/23/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
